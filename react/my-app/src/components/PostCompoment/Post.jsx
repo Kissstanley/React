7dec6f2db0a4078ae16daea64f10a48d50e8e544
Kissstanley <swyatoslawfyk@gmail.com>
2023-07-09 20:17:50 +0300

    complete first.1 task at hw2

--- react/my-app/src/components/PostCompoment/Post.jsx
+++ react/my-app/src/components/PostCompoment/Post.jsx
@@ -1,3 +1,4 @@
+import styles from './Post.module.css'
 import {PostInfo} from "./PostInfo";
 import {useState} from "react";
 
@@ -5,14 +6,14 @@ export const Post=({post})=>{
     const[show,setShow]=useState(false)
     const {id,title}=post;
     return(
-        <>
+        <div className={styles.box}>
             <div>id: {id} </div>
             <div>title: {title} </div>
             <button onClick={()=>setShow(!show)}>{show?'Hide':'Show'}</button>
             {
                 show && <PostInfo post={post}/>
             }
-        </>
+        </div>
     )
 
 }
